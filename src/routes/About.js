4bfad1cda403ebe94e87efb5e472fe299daf0cbd
성웅 <woong@seong-ung-ui-MacBookPro.local>
2020-10-26 17:14:32 +0900

    완성코드

---
+++ src/routes/About.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './About.css'
+function About(props){
+    console.log(props);
+    return (
+        <div className="about__container">
+            <span>
+                "ohhhhohhhohhhohh"
+            </span>
+            <span>- George george,2020</span>
+        </div>
+    )
+}
+
+export default About;
